525a3f475e1d3c533342288aa0e5fac7ed7f19a0
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-04-21 18:46:39 +0200

    First query (it works!)

--- JSONParser/query.py
+++ JSONParser/query.py
@@ -1,19 +1,20 @@
-from parser import *
+from commitDB import *
 from pandas import read_sql
-import sqlite3
+from sqlite3 import connect
 
+database = createDB()
+"""
 print("=== General === \n", csvDf.info)
 print("=== Author === \n", author_pubDf.info)
 print("=== Venues === \n", venueDf.info)
 print("=== References === \n", refDf.info)
 print("=== Publishers === \n", publisherDf.info)
-
 """
+
 def getPublicationsPublishedInYear(year):
     with connect("publications.db") as con:  
         query = "SELECT * FROM General WHERE publication_year = '{0}'".format(year)
         df_sql = read_sql(query, con)
         return df_sql
 
-print(getPublicationsPublishedInYear("2020"))
-"""
+print("First query", getPublicationsPublishedInYear("2020"))

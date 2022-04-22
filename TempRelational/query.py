f9133659be59067101616960e819e7384b893338
ManueleVeggi <manuele.veggi@studio.unibo.it>
2022-04-22 15:53:35 +0200

    Ricevimento

--- TempRelational/query.py
+++ TempRelational/query.py
@@ -3,13 +3,13 @@ from pandas import read_sql
 from sqlite3 import connect
 
 database = createDB()
-"""
+
 print("=== General === \n", csvDf.info)
 print("=== Author === \n", author_pubDf.info)
 print("=== Venues === \n", venueDf.info)
 print("=== References === \n", refDf.info)
 print("=== Publishers === \n", publisherDf.info)
-"""
+
 
 def getPublicationsPublishedInYear(year):
     with connect("publications.db") as con:  

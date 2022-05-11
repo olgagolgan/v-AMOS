8102337ca6a286c4741652fe83e4f5368a23ac2b
olgagolgan <67375574+olgagolgan@users.noreply.github.com>
2022-05-11 10:22:22 +0200

    Update genericQueryProcessor.py

--- genericQueryProcessor.py
+++ genericQueryProcessor.py
@@ -323,12 +323,13 @@ class GenericQueryProcessor:
 # rel_dp.getDbPath()
 # rel_qp = RelationalQueryProcessor(rel_path)
 
-#
 # generic = GenericQueryProcessor([trp_qp, rel_qp])
 
-#TESTER
 
-#1st query
+
+# TESTER
+
+# 1st query
 # my_m1 = generic.getPublicationsPublishedInYear(2020)
 # print(my_m1)
 # print("-----------------------------------")
@@ -336,14 +337,14 @@ class GenericQueryProcessor:
 # print("-----------------------------------")
 
 
-#2nd query
+# 2nd query
 # my_m2 = generic.getPublicationsByAuthorId("0000-0001-7553-6916")
 # print(my_m2)
 # print("-----------------------------------")
 # print(my_m2[1][0].getTitle())
 # print("-----------------------------------")
 
-#3rd query
+# 3rd query
 # my_m3 = generic.getMostCitedPublication()
 # print(my_m3)
 # print("-----------------------------------")
@@ -351,14 +352,14 @@ class GenericQueryProcessor:
 # print(myObj.getCitedPublications())
 # print("-----------------------------------")
 
-#4th query
+# 4th query
 # my_m4 = generic.getMostCitedVenue()
 # print(my_m4)
 # print("-----------------------------------")
 # print(my_m4[1].getTitle())
 # print("-----------------------------------")
 
-#5th query
+# 5th query
 # my_m5 = generic.getVenuesByPublisherId("crossref:78")
 # print(my_m5)
 # print("-----------------------------------")
@@ -366,14 +367,14 @@ class GenericQueryProcessor:
 # print(myObj.getPublisher())
 # print("-----------------------------------")
 
-#6th query
+# 6th query
 # my_m6 = generic.getPublicationInVenue("issn:0944-1344")
 # print(my_m6)
 # print("-----------------------------------")
 # print(my_m6[1][0].getCitedPublications())
 # print("-----------------------------------")
 
-#7th query
+# 7th query
 # my_m7 = generic.getJournalArticlesInIssue(3, 28, "issn:1066-8888")
 # print(my_m7)
 # print("-----------------------------------")
@@ -381,35 +382,35 @@ class GenericQueryProcessor:
 # print("-----------------------------------")
 
 
-#8th query
+# 8th query
 # my_m8 = generic.getJournalArticlesInVolume(28, "issn:1066-8888")
 # print(my_m8)
 # print("-----------------------------------")
 # print (my_m8[1][0].getTitle())
 # print("-----------------------------------")
 
-#9th query
+# 9th query
 # my_m9 = generic.getProceedingsByEvent("")
 # print(my_m9)
 # print("-----------------------------------")
 # print(my_m9[1][0].getEvent())
 # print("-----------------------------------")
 
-#10th query
+# 10th query
 # my_m10 = generic.getPublicationAuthors("doi:10.1007/s11192-019-03311-9")
 # print(my_m10)
 # print("-----------------------------------")
 # print(my_m10[1][0].getGivenName())
 # print("-----------------------------------")
 
-#11th query
+# 11th query
 # my_m11 = generic.getPublicationsByAuthorName("Peroni")
 # print(my_m11)
 # print("-----------------------------------")
 # print(my_m11[1][0].getCitedPublications())
 # print("-----------------------------------")
 
-#12th query
+# 12th query
 # my_m12 = generic.getDistinctPublisherOfPublications([ "doi:10.1080/21645515.2021.1910000", "doi:10.3390/ijfs9030035" ])
 # print(my_m12)
 # print("-----------------------------------")
